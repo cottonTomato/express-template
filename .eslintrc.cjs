cf3a91cb04052e48c352b8cdffe75cbc06de1f41
LanceHoenhymm <abhianv.pandey.1512@gmail.com>
2023-09-22 17:43:45 +0530

    Disabled no-misused-promises rule in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,25 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'prettier'
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
     ],
     overrides: [],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-	},
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
     plugins: ['@typescript-eslint'],
     root: true,
     rules: {
-        '@typescript-eslint/no-unused-vars': [ 'error',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
             {
-                'argsIgnorePattern': '^_',
-                'varsIgnorePattern': '^_'
-            }
-        ]
-    }
-  };
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            },
+        ],
+        '@typescript-eslint/no-misused-promises': 'off',
+    },
+};
