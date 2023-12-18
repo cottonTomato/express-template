26c39e4e94896141f2f8fc2fd5d0ebe1cce63e6d
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-18 19:33:11 +0530

    Update dependencies and imports in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
-import { default as helmet } from 'helmet';
+import helmet from 'helmet';
 import logger from 'morgan';
 
 const port = process.env.PORT ?? 8080;
 const isProduction = process.env.NODE_ENV === 'production';
+
 const app = express();
 
 app.use(express.json());
